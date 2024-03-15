f3d37f718322fbfce890b94e7b97fd38ba723cd6
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 01:25:51 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,4 @@
 const config = {
-    api_url: process.env.API_URL || "https://api.openai-proxy.com/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: process.env.API_KEY || "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"  // openAi apiKey
-};
+    api_url: "https://api.openai-proxy.com/v1/chat/completions",  // gpt3.5代理接口
+    apiKey: "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"  // openAi apiKey
+};  
