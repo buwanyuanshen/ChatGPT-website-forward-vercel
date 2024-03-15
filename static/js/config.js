6da7906002870509b5465c760d52b838f0d47472
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 00:52:07 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,6 @@
 const config = {
-    api_url: process.env.API_URL || "",
-    apiKey: process.env.API_KEY || ""
+    api_url: process.env.API_URL || "https://api.openai-proxy.com/v1/chat/completions",
+    apiKey: process.env.API_KEY || "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"
 };
+
+module.exports = config;
