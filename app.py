0a25882c3e0659108733d3c1ddab7f9579b228de
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 03:53:11 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 import os
 
 app = Flask(__name__)
 
+
+# 设置环境变量
+app.config['API_KEY'] = os.environ.get('API_KEY', '')
+app.config['API_URL'] = os.environ.get('API_URL', '')
+
+@app.route('/config')
+def get_config():
+    return jsonify({
+        'apiKey': app.config['API_KEY'],
+        'apiUrl': app.config['API_URL']
+    }
+
 @app.route('/')
 def index():
-    api_key = os.environ.get('API_KEY', '')
-    api_url = os.environ.get('API_URL', '')
-    return render_template('index.html', api_key=api_key, api_url=api_url)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
