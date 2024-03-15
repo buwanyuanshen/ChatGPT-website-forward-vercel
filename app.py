d1cb585311456de26213a3499801eada744db7aa
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 03:22:15 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask import render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    api_key = os.environ.get('API_KEY', None)
+    api_url = os.environ.get('API_URL', None)
+    return render_template('index.html', api_key=api_key, api_url=api_url)
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
