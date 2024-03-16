4106044a45dca031b0d7bb2c241377f36f958cf1
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 13:21:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
-    api_key = os.environ.get('API_KEY', '')
-    api_url = os.environ.get('API_URL', '')
-    data = {"apiKey": api_key, "api_url": api_url}
-    return render_template('index.html', data=data)
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def index():
+    apiKey = request.form.get("apiKey", None)
+    api_url = request.form.get("api_url", None)
+    if api_url is None:
+        api_url = os.environ.get("API_URL", None)
+    if apiKey is None:
+    apiKey = os.environ.get("API_KEY", None)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
