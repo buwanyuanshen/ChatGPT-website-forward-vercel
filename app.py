32f5ad77e2257f29cc1cc88a9d355d3f5ed49b57
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 13:09:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    api_key = os.environ.get('API_KEY', '')
+    api_url = os.environ.get('API_URL', '')
+    data = {"apiKey": api_key, "api_url": api_url}
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
