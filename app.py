df9c21eee786bc8b2acfaa2f9d5a7ac01aaadec7
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-16 12:59:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
-
-    # 渲染模板时将 apiKey 和 api_url 传递给前端
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', 80)
