21f74be5fee97436b501c677356a83c6eb7859d6
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2025-01-21 03:51:52 +0800

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -879,7 +879,7 @@ if (jsonObj.choices) {
         const content = jsonObj.choices[0].delta.content;
 
         if (reasoningContent && reasoningContent.trim() !== "") {
-            str += reasoningContent + "\n";
+            str += reasoningContent;
         } else if (content && content.trim() !== "") {
             str += content;
         }
@@ -891,7 +891,7 @@ if (jsonObj.choices) {
         const content = message.content;
 
         if (reasoningContent && reasoningContent.trim() !== "") {
-            str += reasoningContent + "\n";
+            str += reasoningContent;
         } else if (content && content.trim() !== "") {
             str += content;
         }
